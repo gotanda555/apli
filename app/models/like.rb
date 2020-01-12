71f3f69248d3d21c89bdb2ed77f3335954ba9866
gotanda555 <crok2558@gmail.com>
2020-01-12 14:59:21 +0900

    レスポンシブ

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,5 @@
 class Like < ApplicationRecord
-  belongs_to :tweet
-  belongs_to :user
+  belongs_to :tweet, dependent: :destroy
+  belongs_to :user, dependent: :destroy
   validates_uniqueness_of :tweet_id, scope: :user_id
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,6 @@
 class Tweet < ApplicationRecord
-  belongs_to :user
-  has_many :comments
-  has_many :likes
-  has_many :liked_users, through: :likes, source: :user
+  belongs_to :user, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :liked_users, through: :likes, source: :user, dependent: :destroy
 end
