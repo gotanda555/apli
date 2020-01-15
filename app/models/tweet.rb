e93f23ac6edef66b720b5e27ec96d4fd7e1e4e44
gotanda555 <crok2558@gmail.com>
2020-01-15 14:56:12 +0900

    テスト 作成

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,5 @@
 class Tweet < ApplicationRecord
-  belongs_to :user, dependent: :destroy
+  belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user, dependent: :destroy
